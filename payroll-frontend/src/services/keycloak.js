c7e299d64c941c771b5909a97ac9ea416a3c9b09
EliasZunigaUSACH <elias.zuniga.t@usach.cl>
2025-12-02 00:52:38 -0300

    update 2 2/12

--- payroll-frontend/src/services/keycloak.js
+++ payroll-frontend/src/services/keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
   url: "http://localhost:9090",
-  realm: "sisgr-realm",
-  clientId: "sisgr-frontend",
+  realm: "toolrent-realm",
+  clientId: "toolrent-frontend",
 });
 
 export default keycloak;
